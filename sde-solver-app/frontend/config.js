b141bdd74d38e7dc3a3c3d96b43ff5d4560bcaca
RAMANAMBONONA <105931833+ramanambonona@users.noreply.github.com>
2025-10-20 10:22:40 +0300

    Update config.js

--- sde-solver-app/frontend/config.js
+++ sde-solver-app/frontend/config.js
@@ -1,12 +1,13 @@
-// Configuration pour les environnements de développement et production
+// Configuration pour les environnements de dÃ©veloppement et production
 const config = {
     development: {
         apiUrl: 'http://localhost:8000'
     },
     production: {
-        apiUrl: 'https://sde-backend-url.railway.app' // Remplacez par votre URL backend
+        apiUrl: 'https://sde-solver-app.onrender.com/' // Remplacez par votre URL backend
     }
 };
 
 const environment = window.location.hostname === 'localhost' ? 'development' : 'production';
+
 export const API_URL = config[environment].apiUrl;
